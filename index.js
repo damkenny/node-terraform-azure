b72e22478cd851804fb528fcfaf904033153cddd
damkenny <adeniyekehinde0@gmail.com>
2022-07-04 12:09:29 +0100

    first commit

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require("body-parser");
 const app = express();
+app.use(express.static(__dirname+'/frontend'));
 app.use(bodyParser.json());
 const  {Client} = require('pg');
-
+const PORT= 3300
 const credentials =  {
     "label": "",
     "host": "kenny.postgres.database.azure.com",
@@ -16,9 +17,30 @@ const credentials =  {
   }
    console.log({credentials});
    const client = new Client (credentials);
-   client.connect();
+  //  client.connect();
 
-client.query('select * from company', (err, res) => {
+   app.get('/', (req, res) => {
+    res.send('connecting to the database')
+  });
+
+  app.get("/url", (req, res, next) => {
+    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+   });
+
+   app.get('/getData', (req, res) => {
+    res.json({
+      "StatusCode": 200, 
+      "StatusMessage": "SUCCESS"
+    })
+   })
+  
+app.listen(PORT, () => console.log(`server is running ON port ${PORT}`));
+
+app.get('/companies', (res, req)=> {
+  client.query(`select * from company`, (err, res) => {
     console.log(err, res)
     client.end()
-  });
+})
+client.connect();
+});
+
